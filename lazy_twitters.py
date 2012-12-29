5799d9092f0e0b25e32c86cf536b4dcde94c71fe
uolter <wtraspad@yahoo.it>
2012-12-29 19:08:24 +0100

    init project

---
+++ lazy_twitters.py
@@ -0,0 +1,60 @@
+'''
+Created on Aug 10, 2012
+
+@author: uolter
+'''
+
+'''
+    look for users who don't tweeting for so long.
+'''
+
+import twitter as tw
+import settings
+from datetime import datetime
+
+consumer_key = settings.consumer_key
+consumer_secret = settings.consumer_secret
+access_token = settings.access_token
+access_token_secret = settings.access_token_secret
+
+api = tw.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
+             access_token_key=access_token, access_token_secret=access_token_secret) 
+
+MY_ID = 32822337
+MY_NAME = u'Walter Traspadini'
+
+
+def str_to_date(str_date):
+    
+    return datetime.strptime(str_date, '%a %b %d %H:%M:%S +0000 %Y')
+
+
+def is_old_days(old_date):
+    
+    today = datetime.today()
+    
+    return (today - old_date).days
+
+def main():
+
+    friend_ids = api.GetFriendIDs()
+
+    for f in friend_ids['ids']:
+        user = api.GetUser(f)
+        try: 
+            # how old was the last post?
+            latest_status = user.status
+            if latest_status:
+                days = is_old_days(str_to_date(latest_status.created_at))
+                
+                if days > 30 *5:
+                    # latest post is more than 6 months old.     
+                    print '%s is more than %d months does not post messages' %(user.name, days/30)
+            else:
+                print '%s never posted messages' %user.name
+                
+        except TypeError:
+            pass
+
+if __name__ == '__main__':
+    main()
